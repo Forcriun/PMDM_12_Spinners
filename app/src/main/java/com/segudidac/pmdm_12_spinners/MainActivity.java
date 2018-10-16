c1970bdfe5f40c5e830708206c35012392901458
Forcriun <forcriun@gmail.com>
2018-10-16 21:19:26 +0200

    Simplificado despues de comprobar arrays.

--- app/src/main/java/com/segudidac/pmdm_12_spinners/MainActivity.java
+++ app/src/main/java/com/segudidac/pmdm_12_spinners/MainActivity.java
@@ -10,10 +10,10 @@ import android.widget.Spinner;
 
 import java.util.ArrayList;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener {
 
-    Spinner sTexto, sFondo;
-    EditText areaTexto;
+    private Spinner sTexto, sFondo;
+    private EditText areaTexto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,90 +25,10 @@ public class MainActivity extends Activity {
         llenadoSpinnerTexto();
         llenadoSpinnerFondo();
 
-        //  Se anaden escuchadores a los desplegables
-        sTexto.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                int idColor = 0;
-                switch (parent.getSelectedItem().toString()) {
-                    case "Blanco":
-                        idColor = getResources().getColor(R.color.colorBlanco);
-                        break;
-                    case "Azul":
-                        idColor = getResources().getColor(R.color.colorAzul);
-                        break;
-                    case "Verde":
-                        idColor = getResources().getColor(R.color.colorVerde);
-                        break;
-                    case "Amarillo":
-                        idColor = getResources().getColor(R.color.colorAmarillo);
-                        break;
-                    case "Rojo":
-                        idColor = getResources().getColor(R.color.colorRojo);
-                        break;
-                    case "Naranja":
-                        idColor = getResources().getColor(R.color.colorNaranja);
-                        break;
-                    case "Marrón":
-                        idColor = getResources().getColor(R.color.colorMarron);
-                        break;
-                    case "Rosa":
-                        idColor = getResources().getColor(R.color.colorRosa);
-                        break;
-                }
-                areaTexto.setTextColor(idColor);
-                //  areaTexto.append(parent.getSelectedItem().toString());
-            }
-
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-        sFondo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                int idColor = 0;
-                switch (parent.getSelectedItem().toString()) {
-                    case "Blanco":
-                        idColor = getResources().getColor(R.color.colorBlanco);
-                        break;
-                    case "Azul":
-                        idColor = getResources().getColor(R.color.colorAzul);
-                        break;
-                    case "Verde":
-                        idColor = getResources().getColor(R.color.colorVerde);
-                        break;
-                    case "Amarillo":
-                        idColor = getResources().getColor(R.color.colorAmarillo);
-                        break;
-                    case "Rojo":
-                        idColor = getResources().getColor(R.color.colorRojo);
-                        break;
-                    case "Naranja":
-                        idColor = getResources().getColor(R.color.colorNaranja);
-                        break;
-                    case "Marrón":
-                        idColor = getResources().getColor(R.color.colorMarron);
-                        break;
-                    case "Rosa":
-                        idColor = getResources().getColor(R.color.colorRosa);
-                        break;
-                }
-                areaTexto.setBackgroundColor(idColor);
-                //  areaTexto.append(parent.getSelectedItem().toString());
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-
+        //  Se anade el escuchador a los spinner
+        sTexto.setOnItemSelectedListener(this);
+        sFondo.setOnItemSelectedListener(this);
     }
-
 
     //  Metodo que se encarga de iniciar y rellenar el desplegable de Texto
     public void llenadoSpinnerTexto() {
@@ -117,7 +37,7 @@ public class MainActivity extends Activity {
         //  Se crea el adaptador encargado de volcar los items en el desplegable
         //  Los items se definen en un array de strings.xml
         ArrayAdapter<CharSequence> adapterTexto = ArrayAdapter.createFromResource(this,
-                R.array.coloresTexto, android.R.layout.simple_spinner_item);
+                R.array.nombresColores, android.R.layout.simple_spinner_item);
         //  Layout por defecto de la lista de opciones que muestra el desplegable
         adapterTexto.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sTexto.setAdapter(adapterTexto);    //  Asignacion del adaptador al desplegable
@@ -126,8 +46,35 @@ public class MainActivity extends Activity {
     public void llenadoSpinnerFondo() {
         sFondo = (Spinner) findViewById(R.id.spinnerFondo);
         ArrayAdapter<CharSequence> adapterFondo = ArrayAdapter.createFromResource(this,
-                R.array.coloresFondo, android.R.layout.simple_spinner_item);
+                R.array.nombresColores, android.R.layout.simple_spinner_item);
         adapterFondo.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sFondo.setAdapter(adapterFondo);
+    }
+
+    //  Metodo que devuelve un array con los IDs de un conjunto de colores predefinido
+    public int[] obtenerColores() {
+        int[] arrayColores = getApplicationContext().getResources().getIntArray(R.array.colores);
+        return arrayColores;
+    }
+
+    //  Implementacion del escuchador
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //  En funcion del spinner sobre el que recae la seleccion...
+        switch (parent.getId()) {
+            case R.id.spinnerTexto:
+                areaTexto.setTextColor(obtenerColores()[position]);
+                //  areaTexto.append(parent.getSelectedItem().toString());
+                break;
+            case R.id.spinnerFondo:
+                areaTexto.setBackgroundColor(obtenerColores()[position]);
+                //  areaTexto.append(parent.getSelectedItem().toString());
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
